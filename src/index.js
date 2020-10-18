b990f83e582473c1fc8546bc896db1025f6b3f70
jiwon_lala <s26788761@naver.com>
2020-10-19 00:01:15 +0900

    \#1 Add Index.js
    cheerio, request 사용하여 사이트의 정보 받아오는 부분 구현

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const URL = "https://sum.su.or.kr:8888/bible/today";
+
+const request = require("request");
+const cheerio = require("cheerio");
+
+const todayBibleObj = {};
+/*
+{
+    date: String; //2020.10.18(일)
+    title: String; //간구하는 소리를 들으소서
+    todayScripture: String; //시편(Psalm) 28:1 ~ 28:9
+    biblicalVerses: String[]; //1. 여호와여 내가 ~
+}
+*/
+
+const getData = () => {
+  request(URL, function (error, response, html) {
+    if (error) console.error(error);
+    if (!error && response.statusCode === 200) {
+      var $ = cheerio.load(html);
+
+      const date = $("div#dailybible_info").text().trim();
+      const title = $("div#bible_text").text().trim();
+      const todayScripture = $("div#bibleinfo_box").text().trim();
+      //const biblicalVerses = $("#body_list").text().trim().replace(/\t/g, "");
+
+      const scriptures = [];
+      $("#body_list")
+        .find("li")
+        .each((index, element) => {
+          //console.log($(element).find(".num").text());
+          //console.log($(element).find(".info").text());
+          const verse = $(element).find(".num").text();
+          const content = $(element).find(".info").text();
+
+          const obj = { verse, content };
+          scriptures.push(obj);
+        });
+
+      console.log(date);
+      console.log(title);
+      console.log(todayScripture);
+      console.log(scriptures);
+    }
+  });
+};
+
+getData();
